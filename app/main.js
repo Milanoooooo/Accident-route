549d2cd09664a242b7451a595688b948e23a063f
VDE <valentin_2107@yahoo.fr>
2019-07-23 14:14:37 +0100

    Paramétrage des barres

--- app/main.js
+++ app/main.js
@@ -4,6 +4,13 @@ console.log('hello', d3);
     try {
         const data = await d3.csv('Data.csv')
         console.log('data', data);
+
+        /*Code pour paramétrer la taille des barres*/
+        const bars = document.querySelectorAll('.bar');
+        bars.forEach((bar, i) => {
+            bar.style.width = `${100* data[i]['nbr_accident']/1000}%`;
+        });
+
     } catch (error) {
         console.log('error', error);
     }
