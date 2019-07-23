df7d05e9771bedb0acd5ee24e3a9d178e9ef1535
VDE <valentin_2107@yahoo.fr>
2019-07-23 15:50:26 +0100

    fin de mise en page

--- app/main.js
+++ app/main.js
@@ -5,36 +5,35 @@ console.log('hello', d3);
         const data = await d3.csv('data/caracteristiques-2017.csv')
         console.log('data', data);
 
-        /* Compte le nombre d'occurrence par département, champ dep dans fichier source 
-        reduce correspond par un groupe by en SQL
-        acc = valeur cumulative par département
-        replace remplace les 0 du fichier initial*/
+        /* Calcul nombre occurence par département,acc va stocker le nbr d'occurence, reduce va faire l'équivalent d'un group by en sql*/
+
         const data2 = data.map(d => d['dep'].replace(/^(.*)0$/, '$1'))
             .reduce((acc, dep) => {
                 acc[dep] = (acc[dep] === undefined) ? 1 : acc[dep] + 1;
                 return acc;
-            }, {})
+            }, {});
         console.log('data2', data2);
 
-        /* on recrée un fichier au format CSV + tri par rapport au nombre du département*/
+        /*Affectation à des noms de colonnes à data3, tri par numéro de département*/
         const data3 = Object.keys(data2)
-            .map(key => ({ departement: key, 'nbr_accident': data2[key] }))
+            .map(key => ({ dept: key, 'nbr-accident': data2[key] }))
             .sort((a, b) => {
-                return a.departement > b.departement ? 1 : -1;
+                return a.dept > b.dept ? 1 : -1;
             });
         console.log('data3', data3);
 
-        /* Code pour rechercher la classe histo et le nombre de bar déclarée (0) et on les associe au jeu de donnée data3 */
-        const bars = d3.select('.histo').selectAll('.bar').data(data3);
+        /*La valeur des bars va dépendre du jeu de donnes data3 */
+        const bars = d3.select('.histo').selectAll('.dept').data(data3);
 
-        /*Création de chaque barre*/
+        /*Pour chaque ligne de valeur détectée on crée une barre*/
         bars.enter()
             .append('div')
-            .classed('bar', true)
-            .style('width', d => (100 * d['nbr_accident'] / 6000) + '%');
+            .classed('dept', true)
+            .html(d => `<span class="dept-num">${d.dept}</span><div class="bar" style="width: ${(100 * d['nbr-accident'] / 6000)}%;"></div>`);
+
 
     } catch (error) {
         console.log('error', error);
     }
-    console.log('etape 3')
+    console.log('coucou');
 })();
