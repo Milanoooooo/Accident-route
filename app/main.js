aef0ca9f4e8580bbc5738da0eb3ca91b348252f3
VDE <valentin_2107@yahoo.fr>
2019-07-23 15:28:19 +0100

    paramétrage des barres

--- app/main.js
+++ app/main.js
@@ -4,17 +4,34 @@ console.log('hello', d3);
     try {
         const data = await d3.csv('data/caracteristiques-2017.csv')
         console.log('data', data);
-        const data2 = data.map(d => d['dep']).reduce((acc, n) => {
-            acc[n] = (acc[n] === undefined) ? 1 : acc[n] + 1;
-            return acc;
-        }, {})
+
+        /* Compte le nombre d'occurrence par département, champ dep dans fichier source 
+        reduce correspond par un groupe by en SQL
+        acc = valeur cumulative par département
+        replace remplace les 0 du fichier initial*/
+        const data2 = data.map(d => d['dep'].replace(/^(.*)0$/, '$1'))
+            .reduce((acc, dep) => {
+                acc[dep] = (acc[dep] === undefined) ? 1 : acc[dep] + 1;
+                return acc;
+            }, {})
         console.log('data2', data2);
 
-        /*Code pour paramétrer la taille des barres, i sera chaque ligne du fichier*/
-        const bars = document.querySelectorAll('.bar');
-        bars.forEach((bar, i) => {
-            bar.style.width = `${100* data[i]['nbr_accident']/1000}%`;
-        });
+        /* on recrée un fichier au format CSV + tri par rapport au nombre du département*/
+        const data3 = Object.keys(data2)
+            .map(key => ({ departement: key, 'nbr_accident': data2[key] }))
+            .sort((a, b) => {
+                return a.departement > b.departement ? 1 : -1;
+            });
+        console.log('data3', data3);
+
+        /* Code pour rechercher la classe histo et le nombre de bar déclarée (0) et on les associe au jeu de donnée data3 */
+        const bars = d3.select('.histo').selectAll('.bar').data(data3);
+
+        /*Création de chaque barre*/
+        bars.enter()
+            .append('div')
+            .classed('bar', true)
+            .style('width', d => (100 * d['nbr_accident'] / 6000) + '%');
 
     } catch (error) {
         console.log('error', error);
